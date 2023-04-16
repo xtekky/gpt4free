69593b77b1bdd2f20d1275a64326d6abc92082e7
t.me/xtekky <98614666+xtekky@users.noreply.github.com>
2023-04-16 14:28:21 +0100

    Revert "unpatch mail"

--- quora/mail.py
+++ quora/mail.py
@@ -7,32 +7,56 @@ class Mail:
         self.client = Session()
         self.client.proxies = None #proxies
         self.client.headers = {
+            "host": "api.mail.tm",
+            "connection": "keep-alive",
+            "sec-ch-ua": "\"Google Chrome\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"",
+            "accept": "application/json, text/plain, */*",
+            "content-type": "application/json",
+            "sec-ch-ua-mobile": "?0",
             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
-            "Accept": "application/json",
-            "Content-Type": "application/json"
+            "sec-ch-ua-platform": "\"macOS\"",
+            "origin": "https://mail.tm",
+            "sec-fetch-site": "same-site",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-dest": "empty",
+            "referer": "https://mail.tm/",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8"
         }
         
-        self.domain = "guerrillamail.com"
-        
-        self.sid_token = self.client.get("https://api.guerrillamail.com/ajax.php?f=get_email_address").json()['sid_token']
-
     def get_mail(self) -> str:
         token = ''.join(choices(ascii_letters, k=10)).lower()
-
-        email_id = f"{token}@{self.domain}"
-        self.client.get(f"https://api.guerrillamail.com/ajax.php?f=set_email_user&email_user={token}&sid_token={self.sid_token}")
-       
-        return email_id
-
+        
+        init = self.client.post("https://api.mail.tm/accounts", json={
+            "address" : f"{token}@bugfoo.com",
+            "password": token
+        })
+        
+        if init.status_code == 201:
+            resp = self.client.post("https://api.mail.tm/token", json = {
+                **init.json(),
+                "password": token
+            })
+            
+            self.client.headers['authorization'] = 'Bearer ' + resp.json()['token']
+            
+            return f"{token}@bugfoo.com"
+        
+        else:
+            raise Exception("Failed to create email")
+        
     def fetch_inbox(self):
-        return self.client.get(f"https://api.guerrillamail.com/ajax.php?f=get_emails&sid_token={self.sid_token}").json()
+        return self.client.get(f"https://api.mail.tm/messages").json()["hydra:member"]
 
     def get_message(self, message_id: str):
-        return self.client.get(f"https://api.guerrillamail.com/ajax.php?f=fetch_email&email_id={message_id}&sid_token={self.sid_token}").json()
+        return self.client.get(f"https://api.mail.tm/messages/{message_id}").json()
 
     def get_message_content(self, message_id: str):
-        return self.get_message(message_id)["mail_body"]
+        return self.get_message(message_id)["text"]
+
 
 # if __name__ == "__main__":
 #     client = Mail()
-#    client.get_mail()
+#     client.get_mail()
+    
+    
