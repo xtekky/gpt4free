91d344b003a2b57b62f076b131681933f78954b0
wingeva1986 <35324882+wingeva1986@users.noreply.github.com>
2023-04-21 18:52:28 +0800

    Update app.py
    dddd

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def poeconv():
 
 def ask(model,prompt,token):
     for response in quora.StreamingCompletion.create(model=model, prompt=prompt, token=token):
-        r=response.completion.choices[0].text.replace("\r\n","\n")
+        r=json.dumps(response.completion.choices[0].text)#.replace("\n","\r")
         yield f'"{r}"'
 
 @app.route('/')
