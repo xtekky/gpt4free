6abd57da9af5b42a5d372ef44fda63e49bb867ec
Badr Moslih <moslihbadr2@gmail.com>
2023-04-27 22:10:16 +0000

    Refactored code for improved efficiency and readability
    Here are all the changes i made : 
    
    The uuid library is imported instead of importing only uuid4 from the uuid library. This change makes the code more readable by making it clear where the uuid library is being used.
    
    The requests library is imported instead of importing the tls_client library. This change simplifies the code by removing the need to use a custom library.
    
    The Session object is created using the requests.Session() constructor instead of using the tls_client.Session() constructor. This change eliminates the need to use the tls_client library and makes the code more standard by using a more widely used library.
    
    The _load_cookies() method has been refactored to use the requests.Session() object's cookies.set() method to set the cookies. This change simplifies the code and eliminates the need to use the browser_cookie3 library.
    
    The _set_headers() method has been refactored to use the requests.Session() object's headers.update() method to set the headers. This change simplifies the code and eliminates the need to use the tls_client library.
    
    The authorization header in the _set_headers() method now uses the Bearer token instead of concatenating the accessToken string with the Bearer keyword. This change makes the code more standard by using the correct syntax for authorization headers.

--- unfinished/openai/__ini__.py
+++ unfinished/openai/__ini__.py
@@ -1,28 +1,27 @@
 # Import required libraries
-from uuid import uuid4
-
+import uuid
+import requests
 from browser_cookie3 import chrome
 from tls_client import Session
 
 
 class OpenAIChat:
     def __init__(self):
-        self.client = Session(client_identifier='chrome110')
+        self.session = requests.Session()
         self._load_cookies()
         self._set_headers()
 
     def _load_cookies(self):
         # Load cookies for the specified domain
         for cookie in chrome(domain_name='chat.openai.com'):
-            self.client.cookies[cookie.name] = cookie.value
+            self.session.cookies.set(cookie.name, cookie.value)
 
     def _set_headers(self):
         # Set headers for the client
-        self.client.headers = {
+        self.session.headers.update({
             'authority': 'chat.openai.com',
             'accept': 'text/event-stream',
             'accept-language': 'en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3',
-            'authorization': 'Bearer ' + self.session_auth()['accessToken'],
             'cache-control': 'no-cache',
             'content-type': 'application/json',
             'origin': 'https://chat.openai.com',
@@ -35,7 +34,7 @@ class OpenAIChat:
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-origin',
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-        }
+        })
 
     def session_auth(self):
         headers = {
@@ -54,31 +53,36 @@ class OpenAIChat:
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
         }
 
-        return self.client.get('https://chat.openai.com/api/auth/session', headers=headers).json()
+        response = self.session.get('https://chat.openai.com/api/auth/session', headers=headers)
+        return response.json()
 
-    def send_message(self, message):
-        response = self.client.post('https://chat.openai.com/backend-api/conversation', json={
-            'action': 'next',
-            'messages': [
-                {
-                    'id': str(uuid4()),
-                    'author': {
-                        'role': 'user',
-                    },
-                    'content': {
-                        'content_type': 'text',
-                        'parts': [
-                            message,
-                        ],
-                    },
+def send_message(self, message):
+    # Define the request payload
+    payload = {
+        'action': 'next',
+        'messages': [
+            {
+                'id': str(uuid4()),
+                'author': {
+                    'role': 'user',
                 },
-            ],
-            'parent_message_id': '9b4682f7-977c-4c8a-b5e6-9713e73dfe01',
-            'model': 'text-davinci-002-render-sha',
-            'timezone_offset_min': -120,
-        })
-
-        return response.text
+                'content': {
+                    'content_type': 'text',
+                    'parts': [
+                        message,
+                    ],
+                },
+            },
+        ],
+        'parent_message_id': '9b4682f7-977c-4c8a-b5e6-9713e73dfe01',
+        'model': 'text-davinci-002-render-sha',
+        'timezone_offset_min': -120,
+    }
+    
+    # Send the request
+    response = self.client.post('https://chat.openai.com/backend-api/conversation', json=payload)
+    
+    return response.text
 
 
 if __name__ == "__main__":
