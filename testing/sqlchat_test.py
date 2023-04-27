1704cbebc8713b9a29e71db310b6cf2b5cf13bcf
t.me/xtekky <98614666+xtekky@users.noreply.github.com>
2023-04-27 19:15:49 +0100

    _

--- testing/sqlchat_test.py
+++
@@ -1,7 +0,0 @@
-import sqlchat
-
-for response in sqlchat.StreamCompletion.create(
-    prompt   = 'write python code to reverse a string',
-    messages = []):
-
-    print(response.completion.choices[0].text, end='')
