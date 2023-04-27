d72c33b680b25d353f5cdb4f77fd3429e22cb73b
Andrew Tsegaye <andrewtsegaye7@gmail.com>
2023-04-26 18:12:33 -0700

    new changes

--- gui/streamlit_app.py
+++ gui/streamlit_app.py
@@ -1,25 +1,29 @@
 import streamlit as st
 import phind
 
-phind.cf_clearance = ''
-phind.user_agent   = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+# Set cloudflare clearance and user agent
+phind.cloudflare_clearance = ''
+phind.phind_api = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
 
-def phind_get_answer(question:str)->str:
-    # set cf_clearance cookie
+
+def get_answer(question: str) -> str:
+    # Set cloudflare clearance cookie and get answer from GPT-4 model
     try:
-    
         result = phind.Completion.create(
-        model  = 'gpt-4',
-        prompt = question,
-        results     = phind.Search.create(question, actualSearch = True),
-        creative    = False,
-        detailed    = False,
-        codeContext = '') 
+            model='gpt-4',
+            prompt=question,
+            results=phind.Search.create(question, actualSearch=True),
+            creative=False,
+            detailed=False,
+            codeContext=''
+        )
         return result.completion.choices[0].text
-
     except Exception as e:
-        return 'An error occured, please make sure you are using a cf_clearance token and correct useragent | %s' % e
+        # Return error message if an exception occurs
+        return f'An error occurred: {e}. Please make sure you are using a valid cloudflare clearance token and user agent.'
 
+
+# Set page configuration and add header
 st.set_page_config(
     page_title="gpt4freeGUI",
     initial_sidebar_state="expanded",
@@ -30,19 +34,21 @@ st.set_page_config(
         'About': "### gptfree GUI"
     }
 )
-
 st.header('GPT4free GUI')
 
-question_text_area = st.text_area('🤖 Ask Any Question :', placeholder='Explain quantum computing in 50 words')
+# Add text area for user input and button to get answer
+question_text_area = st.text_area(
+    '🤖 Ask Any Question :', placeholder='Explain quantum computing in 50 words')
 if st.button('🧠 Think'):
-    answer = phind_get_answer(question_text_area)
+    answer = get_answer(question_text_area)
+    # Display answer
     st.caption("Answer :")
     st.markdown(answer)
 
-
+# Hide Streamlit footer
 hide_streamlit_style = """
             <style>
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
