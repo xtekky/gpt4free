8c1d2a9b657c70cc1e800e4d6e827e68408bda21
Andrew Tsegaye <91322467+Andrew-Tsegaye@users.noreply.github.com>
2023-04-30 11:22:58 -0700

    Notable changes included

--- quora/mail.py
+++ quora/mail.py
@@ -1,6 +1,6 @@
-from json import loads
-from re import findall
-from time import sleep
+import json
+import re
+import time
 
 from fake_useragent import UserAgent
 from requests import Session
@@ -8,75 +8,73 @@ from requests import Session
 
 class Emailnator:
     def __init__(self) -> None:
-        self.client = Session()
-        self.client.get("https://www.emailnator.com/", timeout=6)
-        self.cookies = self.client.cookies.get_dict()
+        self.session = requests.Session()
+        self._initialize_session()
 
-        self.client.headers = {
+        self.email_address = None
+
+    def _initialize_session(self):
+        self.session.get("https://www.emailnator.com/", timeout=6)
+        self.session.headers = {
             "authority": "www.emailnator.com",
             "origin": "https://www.emailnator.com",
             "referer": "https://www.emailnator.com/",
             "user-agent": UserAgent().random,
-            "x-xsrf-token": self.client.cookies.get("XSRF-TOKEN")[:-3] + "=",
+            "x-xsrf-token": self.session.cookies.get("XSRF-TOKEN")[:-3] + "=",
         }
 
-        self.email = None
-
-    def get_mail(self):
-        response = self.client.post(
+    def generate_email_address(self):
+        response = self.session.post(
             "https://www.emailnator.com/generate-email",
-            json={
-                "email": [
-                    "domain",
-                    "plusGmail",
-                    "dotGmail",
-                ]
-            },
+            json={"email": ["domain", "plusGmail", "dotGmail"]},
         )
+        self.email_address = json.loads(response.text)["email"][0]
+        return self.email_address
 
-        self.email = loads(response.text)["email"][0]
-        return self.email
-
-    def get_message(self):
+    def _wait_for_message(self):
         print("Waiting for message...")
 
         while True:
-            sleep(2)
-            mail_token = self.client.post(
-                "https://www.emailnator.com/message-list", json={"email": self.email}
+            time.sleep(2)
+            mail_token_response = self.session.post(
+                "https://www.emailnator.com/message-list",
+                json={"email": self.email_address},
             )
 
-            mail_token = loads(mail_token.text)["messageData"]
+            mail_token = json.loads(mail_token_response.text)["messageData"]
 
             if len(mail_token) == 2:
                 print("Message received!")
                 print(mail_token[1]["messageID"])
                 break
 
-        mail_context = self.client.post(
+        return mail_token[1]["messageID"]
+
+    def get_message_content(self):
+        message_id = self._wait_for_message()
+
+        mail_context_response = self.session.post(
             "https://www.emailnator.com/message-list",
-            json={
-                "email": self.email,
-                "messageID": mail_token[1]["messageID"],
-            },
+            json={"email": self.email_address, "messageID": message_id},
         )
 
-        return mail_context.text
+        return mail_context_response.text
 
     def get_verification_code(self):
-        message = self.get_message()
-        code = findall(r';">(\d{6,7})</div>', message)[0]
-        print(f"Verification code: {code}")
-        return code
+        message_content = self.get_message_content()
+        verification_code = re.findall(
+            r';">(\d{6,7})</div>', message_content)[0]
+        print(f"Verification code: {verification_code}")
+        return verification_code
 
     def clear_inbox(self):
         print("Clearing inbox...")
-        self.client.post(
+        self.session.post(
             "https://www.emailnator.com/delete-all",
-            json={"email": self.email},
+            json={"email": self.email_address},
         )
         print("Inbox cleared!")
 
     def __del__(self):
-        if self.email:
+        if self.email_address:
             self.clear_inbox()
