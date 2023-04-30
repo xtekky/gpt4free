e71e0e1d681daac84691b1496329b638f6cf98f2
Andrew Tsegaye <91322467+Andrew-Tsegaye@users.noreply.github.com>
2023-04-30 10:48:23 -0700

    Modular & Readable Code

--- forefront/mail.py
+++ forefront/mail.py
@@ -1,12 +1,20 @@
+import json
 from requests import Session
 from string import ascii_letters
 from random import choices
 
+
 class Mail:
+    BASE_URL = "https://api.mail.tm"
+
     def __init__(self, proxies: dict = None) -> None:
-        self.client = Session()
-        self.client.proxies = proxies
-        self.client.headers = {
+        self.client = self._create_session(proxies)
+
+    @staticmethod
+    def _create_session(proxies: dict = None) -> Session:
+        session = Session()
+        session.proxies = proxies
+        session.headers = {
             "host": "api.mail.tm",
             "connection": "keep-alive",
             "sec-ch-ua": "\"Google Chrome\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"",
@@ -23,33 +31,38 @@ class Mail:
             "accept-encoding": "gzip, deflate, br",
             "accept-language": "en-GB,en-US;q=0.9,en;q=0.8"
         }
-        
+        return session
+
+    @staticmethod
+    def _generate_token(length: int = 14) -> str:
+        return ''.join(choices(ascii_letters, k=length)).lower()
+
+    def _request(self, method: str, endpoint: str, **kwargs) -> dict:
+        response = self.client.request(
+            method, f"{self.BASE_URL}/{endpoint}", **kwargs)
+        response.raise_for_status()
+        return response.json()
+
     def get_mail(self) -> str:
-        token = ''.join(choices(ascii_letters, k=14)).lower()
-        init  = self.client.post("https://api.mail.tm/accounts", json={
-            "address" : f"{token}@bugfoo.com",
+        token = self._generate_token()
+        init = self._request("post", "accounts", json={
+            "address": f"{token}@bugfoo.com",
             "password": token
         })
-        
-        if init.status_code == 201:
-            resp = self.client.post("https://api.mail.tm/token", json = {
-                **init.json(),
-                "password": token
-            })
-            
-            self.client.headers['authorization'] = 'Bearer ' + resp.json()['token']
-            
-            return f"{token}@bugfoo.com"
-        
-        else:
-            raise Exception("Failed to create email")
-    
+
+        resp = self._request("post", "token", json={
+            **init,
+            "password": token
+        })
+
+        self.client.headers['authorization'] = f"Bearer {resp['token']}"
+        return f"{token}@bugfoo.com"
+
     def fetch_inbox(self):
-        return self.client.get(f"https://api.mail.tm/messages").json()["hydra:member"]
+        return self._request("get", "messages")["hydra:member"]
 
     def get_message(self, message_id: str):
-        return self.client.get(f"https://api.mail.tm/messages/{message_id}").json()
+        return self._request("get", f"messages/{message_id}")
 
     def get_message_content(self, message_id: str):
         return self.get_message(message_id)["text"]
-
