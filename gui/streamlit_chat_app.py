f0695395e95968f120a3272b32840de96760a56e
Andrew Tsegaye <91322467+Andrew-Tsegaye@users.noreply.github.com>
2023-04-30 16:15:40 -0700

    Highly refactored object-oriented code

--- gui/streamlit_chat_app.py
+++ gui/streamlit_chat_app.py
@@ -1,72 +1,58 @@
 import atexit
 import os
 import sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
-
+import pickle
 import streamlit as st
 from streamlit_chat import message
 from query_methods import query, avail_query_methods
-import pickle
 
-conversations_file = "conversations.pkl"
-
-def load_conversations():
-    try:
-        with open(conversations_file, "rb") as f:
-            return pickle.load(f)
-    except FileNotFoundError:
-        return []
-    except EOFError:
-        return []
+sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
 
-def save_conversations(conversations, current_conversation):
-    updated = False
-    for idx, conversation in enumerate(conversations):
-        if conversation == current_conversation:
-            conversations[idx] = current_conversation
-            updated = True
-            break
-    if not updated:
-        conversations.append(current_conversation)
+class ConversationManager:
+    def __init__(self, conversations_file="conversations.pkl"):
+        self.conversations_file = conversations_file
+        self.conversations = self.load_conversations()
 
-    temp_conversations_file = "temp_" + conversations_file
-    with open(temp_conversations_file, "wb") as f:
-        pickle.dump(conversations, f)
+    def load_conversations(self):
+        try:
+            with open(self.conversations_file, "rb") as f:
+                return pickle.load(f)
+        except (FileNotFoundError, EOFError):
+            return []
 
-    os.replace(temp_conversations_file, conversations_file)
+    def save_conversations(self):
+        temp_conversations_file = "temp_" + self.conversations_file
+        with open(temp_conversations_file, "wb") as f:
+            pickle.dump(self.conversations, f)
+        os.replace(temp_conversations_file, self.conversations_file)
+
+    def update_conversation(self, current_conversation):
+        updated = False
+        for idx, conversation in enumerate(self.conversations):
+            if conversation == current_conversation:
+                self.conversations[idx] = current_conversation
+                updated = True
+                break
+        if not updated:
+            self.conversations.append(current_conversation)
+
+    def exit_handler(self):
+        print("Exiting, saving data...")
+        self.save_conversations()
 
 
-def exit_handler():
-    print("Exiting, saving data...")
-    # Perform cleanup operations here, like saving data or closing open files.
-    save_conversations(st.session_state.conversations, st.session_state.current_conversation)
-
-
-# Register the exit_handler function to be called when the program is closing.
-atexit.register(exit_handler)
+conversation_manager = ConversationManager()
+atexit.register(conversation_manager.exit_handler)
 
 st.header("Chat Placeholder")
 
-if 'conversations' not in st.session_state:
-    st.session_state['conversations'] = load_conversations()
-
-if 'input_text' not in st.session_state:
-    st.session_state['input_text'] = ''
-
-if 'selected_conversation' not in st.session_state:
-    st.session_state['selected_conversation'] = None
-
-if 'input_field_key' not in st.session_state:
-    st.session_state['input_field_key'] = 0
-
-if 'query_method' not in st.session_state:
-    st.session_state['query_method'] = query
-
-# Initialize new conversation
-if 'current_conversation' not in st.session_state or st.session_state['current_conversation'] is None:
-    st.session_state['current_conversation'] = {'user_inputs': [], 'generated_responses': []}
+st.session_state.setdefault('input_text', '')
+st.session_state.setdefault('selected_conversation', None)
+st.session_state.setdefault('input_field_key', 0)
+st.session_state.setdefault('query_method', query)
+st.session_state.setdefault('current_conversation', {
+                            'user_inputs': [], 'generated_responses': []})
 
 input_placeholder = st.empty()
 user_input = input_placeholder.text_input(
@@ -74,45 +60,47 @@ user_input = input_placeholder.text_input(
 )
 submit_button = st.button("Submit")
 
-
 if (user_input and user_input != st.session_state['input_text']) or submit_button:
     output = query(user_input, st.session_state['query_method'])
 
     escaped_output = output.encode('utf-8').decode('unicode-escape')
 
     st.session_state.current_conversation['user_inputs'].append(user_input)
-    st.session_state.current_conversation['generated_responses'].append(escaped_output)
-    save_conversations(st.session_state.conversations, st.session_state.current_conversation)
+    st.session_state.current_conversation['generated_responses'].append(
+        escaped_output)
+    conversation_manager.update_conversation(
+        st.session_state.current_conversation)
     st.session_state['input_text'] = ''
     user_input = input_placeholder.text_input(
         'You:', value=st.session_state['input_text'], key=f'input_text_{st.session_state["input_field_key"]}'
-    )  # Clear the input field
+    )
 
-# Add a button to create a new conversation
 if st.sidebar.button("New Conversation"):
     st.session_state['selected_conversation'] = None
-    st.session_state['current_conversation'] = {'user_inputs': [], 'generated_responses': []}
+    st.session_state['current_conversation'] = {
+        'user_inputs': [], 'generated_responses': []}
     st.session_state['input_field_key'] += 1
 
-st.session_state['query_method'] = st.sidebar.selectbox("Select API:", options=avail_query_methods, index=0)
-
-# Proxy
+st.session_state['query_method'] = st.sidebar.selectbox(
+    "Select API:", options=avail_query_methods, index=0)
 st.session_state['proxy'] = st.sidebar.text_input("Proxy: ")
 
-# Sidebar
 st.sidebar.header("Conversation History")
 
-for idx, conversation in enumerate(st.session_state.conversations):
+for idx, conversation in enumerate(conversation_manager.conversations):
     if st.sidebar.button(f"Conversation {idx + 1}: {conversation['user_inputs'][0]}", key=f"sidebar_btn_{idx}"):
         st.session_state['selected_conversation'] = idx
-        st.session_state['current_conversation'] = st.session_state.conversations[idx]
+        st.session_state['current_conversation'] = conversation_manager.conversations[idx]
 
-if st.session_state['selected_conversation'] is not None:
-    conversation_to_display = st.session_state.conversations[st.session_state['selected_conversation']]
-else:
-    conversation_to_display = st.session_state.current_conversation
+conversation_to_display = (
+    st.session_state.current_conversation
+    if st.session_state['selected_conversation'] is None
+    else conversation_manager.conversations[st.session_state['selected_conversation']]
+)
 
 if conversation_to_display['generated_responses']:
     for i in range(len(conversation_to_display['generated_responses']) - 1, -1, -1):
-        message(conversation_to_display["generated_responses"][i], key=f"display_generated_{i}")
-        message(conversation_to_display['user_inputs'][i], is_user=True, key=f"display_user_{i}")
+        message(
+            conversation_to_display["generated_responses"][i], key=f"display_generated_{i}")
+        message(conversation_to_display['user_inputs']
+                [i], is_user=True, key=f"display_user_{i}")
