d27c48bb30d25f7ad56830524071f19c4a0c6c9a
Andrew Tsegaye <91322467+Andrew-Tsegaye@users.noreply.github.com>
2023-05-01 07:37:53 -0700

    customized code

--- gui/streamlit_app.py
+++ gui/streamlit_app.py
@@ -4,8 +4,8 @@ import os
 import sys
 from typing import Union
 
+# Add parent directory to the system path
 sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
-
 
 class GPT4FreeModel:
     def __init__(self):
@@ -18,14 +18,13 @@ class GPT4FreeModel:
         except Exception as e:
             return None, e
 
-
 def display_error_message(e: Exception) -> str:
     return (
         f'An error occurred: {e}. Please make sure you are using a valid cloudflare clearance token and user agent.'
     )
 
-
 def main():
+    # Set up Streamlit page configuration
     st.set_page_config(
         page_title="gpt4freeGUI",
         initial_sidebar_state="expanded",
@@ -36,14 +35,20 @@ def main():
             'About': "### gptfree GUI",
         },
     )
+    
+    # Display the header
     st.header('GPT4free GUI')
 
+    # Add a text area for users to input their question
     question_text_area = st.text_area(
         '🤖 Ask Any Question :', placeholder='Explain quantum computing in 50 words')
+    
     gpt4free_model = GPT4FreeModel()
 
+    # Process the question when the "Think" button is clicked
     if st.button('🧠 Think'):
         answer, error = gpt4free_model.get_answer(question_text_area)
+        
         if answer:
             escaped = answer.encode('utf-8').decode('unicode-escape')
             st.caption("Answer :")
@@ -51,13 +56,13 @@ def main():
         else:
             st.error(display_error_message(error))
 
+    # Hide Streamlit footer
     hide_streamlit_style = """
                 <style>
                 footer {visibility: hidden;}
                 </style>
                 """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
 
 if __name__ == "__main__":
     main()
