86088bef83fb70cbc84fd2475446ca9fb79c460b
Bagus Indrayana <bagusindrayanaindo@gmail.com>
2023-07-12 01:13:45 +0800

    update testing

--- testing/binghuan/testing.py
+++ testing/binghuan/testing.py
@@ -1,14 +1,12 @@
 from BingHuan import ChatCompletion
-# text = "Hello, this is Bing. I can help you find information on the web, generate content such as poems, stories, code, essays, songs, celebrity parodies and more. I can also help you with rewriting, improving, or optimizing your content. Is there anything specific you would like me to help you with? 😊"
-# print(text.encode('utf-8'))
 
-# # Test 1
-# response = ChatCompletion.create(model="gpt-3.5-turbo",
-#                                  provider="BingHuan",
-#                                  stream=False,
-#                                  messages=[{'role': 'user', 'content': 'who are you?'}])
+# Test 1
+response = ChatCompletion.create(model="gpt-3.5-turbo",
+                                 provider="BingHuan",
+                                 stream=False,
+                                 messages=[{'role': 'user', 'content': 'who are you?'}])
 
-# print(response)
+print(response)
 
 # Test 2
 response = ChatCompletion.create(model="gpt-3.5-turbo",
@@ -19,14 +17,14 @@ response = ChatCompletion.create(model="gpt-3.5-turbo",
 print(response)
 
 
-# Test 3
-# response = ChatCompletion.create(model="gpt-4",
-#                                  provider="BingHuan",
-#                                  stream=False,
-#                                  messages=[
-#                                      {'role': 'user', 'content': 'now your name is Bob'},
-#                                      {'role': 'assistant', 'content': 'Hello Im Bob, you asistant'},
-#                                      {'role': 'user', 'content': 'what your name again?'},
-#                                  ])
+Test 3
+response = ChatCompletion.create(model="gpt-4",
+                                 provider="BingHuan",
+                                 stream=False,
+                                 messages=[
+                                     {'role': 'user', 'content': 'now your name is Bob'},
+                                     {'role': 'assistant', 'content': 'Hello Im Bob, you asistant'},
+                                     {'role': 'user', 'content': 'what your name again?'},
+                                 ])
 
-# print(response)
+print(response)
