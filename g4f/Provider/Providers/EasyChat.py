6f535ac3e8851fc42beba9cdc68d61d75c771ca7
omidima <47784584+omidima@users.noreply.github.com>
2023-08-08 12:19:27 +0330

    Update EasyChat.py
    Change model address and add proxy & session for pass cloudflare auth on requests.

--- g4f/Provider/Providers/EasyChat.py
+++ g4f/Provider/Providers/EasyChat.py
@@ -10,38 +10,39 @@ working = True
 
 
 def _create_completion(model: str, messages: list, stream: bool, **kwargs):
+    req = requests.Session()
+    
+    proxy = {
+        "http": "http://159.89.138.130:80"
+    }
+    
     headers = {
-        'authority': 'free.easychat.work',
+        'authority': 'beta.easychat.work',
         'accept': 'text/event-stream',
-        'accept-language': 'en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3',
+        'accept-language': 'en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3,fa=0.2',
         'content-type': 'application/json',
-        'endpoint': '',
-        'origin': 'https://free.easychat.work',
-        'plugins': '0',
-        'referer': 'https://free.easychat.work/',
+        'origin': 'https://beta.easychat.work',
+        'referer': 'https://beta.easychat.work/',
         'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-        'usesearch': 'false',
         'x-requested-with': 'XMLHttpRequest',
     }
 
     json_data = {
         'messages': messages,
         'stream': True,
-        'model': model,
+        'model': "gpt-3.5-turbo",
         'temperature': kwargs.get('temperature', 0.5),
         'presence_penalty': kwargs.get('presence_penalty', 0),
         'frequency_penalty': kwargs.get('frequency_penalty', 0),
         'top_p': kwargs.get('top_p', 1),
     }
 
-    response = requests.post('https://free.easychat.work/api/openai/v1/chat/completions',
-        headers=headers, json=json_data)
+    # init cookies from server
+    req.get("https://site.easygpt.work/", proxies=proxy)
+
+    response = req.post('https://beta.easychat.work/api/openai/v1/chat/completions',
+        headers=headers, json=json_data, proxies=proxy)
     
     for chunk in response.iter_lines():
         if b'content' in chunk:
