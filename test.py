d62dd7fbb3694ea60000e7c3bbc004a1032e65f4
gdrive9532 <133782542+gdrive9532@users.noreply.github.com>
2023-08-24 14:10:33 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,36 @@
+import g4f
+
+
+print(g4f.Provider.Ails.params)  # supported args
+
+# Automatic selection of provider
+
+# streamed completion
+response = g4f.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Hello world"}],
+    stream=True,
+)
+
+for message in response:
+    print(message, flush=True, end='')
+
+# normal response
+response = g4f.ChatCompletion.create(
+    model=g4f.models.gpt_4,
+    messages=[{"role": "user", "content": "hi"}],
+)  # alterative model setting
+
+print(response)
+
+
+# Set with provider
+response = g4f.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    provider=g4f.Provider.DeepAi,
+    messages=[{"role": "user", "content": "Hello world"}],
+    stream=True,
+)
+
+for message in response:
+    print(message)
