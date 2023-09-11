8ad46284b9b12af43d542f4751975c4d5911e1ef
hs_junxiang <jimmy871117@gmail.com>
2023-09-11 15:29:48 +0800

    Fix: `TypedDict` from `typing` error #886
    Ref: https://github.com/python/typeshed/issues/3500

--- g4f/typing.py
+++ g4f/typing.py
@@ -1,4 +1,10 @@
-from typing import Any, AsyncGenerator, Generator, NewType, Tuple, TypedDict, Union
+import sys
+from typing import Any, AsyncGenerator, Generator, NewType, Tuple, Union
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
 
 SHA256 = NewType('sha_256_hash', str)
 CreateResult = Generator[str, None, None]
