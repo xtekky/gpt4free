261e14fa5b1ada40319d5e1885ff2a213df6f026
abc <98614666+xtekky@users.noreply.github.com>
2023-09-12 21:25:11 +0100

    ~ | v.0.0.2.9 - new providers

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("requirements.txt") as f:
 with open("interference/requirements.txt") as f:
     api_required = f.read().splitlines()
 
-VERSION = '0.0.2.8'
+VERSION = '0.0.2.9'
 DESCRIPTION = (
     "The official gpt4free repository | various collection of powerful language models"
 )
