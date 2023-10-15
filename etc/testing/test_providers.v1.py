46398e8ac643c90e68d04be77a63379aef5df6ec
abc <98614666+xtekky@users.noreply.github.com>
2023-10-16 00:46:58 +0100

    ~ | new `test_providers.py`

--- etc/testing/test_providers.v1.py
+++ etc/testing/test_providers.v1.py
@@ -1,36 +1,3 @@
-# from g4f.Provider import __all__, ProviderUtils
-# from g4f import ChatCompletion
-# import concurrent.futures
-
-# _ = [
-#     'BaseProvider',
-#     'AsyncProvider',
-#     'AsyncGeneratorProvider',
-#     'RetryProvider'
-# ]
-
-# def test_provider(provider):
-#     try:
-#         provider = (ProviderUtils.convert[provider])
-#         if provider.working and not provider.needs_auth:
-#             print('testing', provider.__name__)
-#             completion = ChatCompletion.create(model='gpt-3.5-turbo', 
-#                                             messages=[{"role": "user", "content": "hello"}], provider=provider)
-#             return completion, provider.__name__
-#     except Exception as e:
-#         #print(f'Failed to test provider: {provider} | {e}')
-#         return None
-
-# with concurrent.futures.ThreadPoolExecutor() as executor:
-#     futures = []
-#     for provider in __all__:
-#         if provider not in _:
-#             futures.append(executor.submit(test_provider, provider))
-#     for future in concurrent.futures.as_completed(futures):
-#         result = future.result()
-#         if result:
-#             print(f'{result[1]} | {result[0]}')
-
 import sys
 from pathlib import Path
 from colorama import Fore, Style
