03dc8532f9c83edcec0214f83445028f4766cf6f
unknown <alexey.verb.04@gmail.com>
2023-10-27 23:35:10 +0300

    added context

--- g4f/gui/server/backend.py
+++ g4f/gui/server/backend.py
@@ -1,5 +1,4 @@
 import g4f
-import json
 
 from flask      import request
 from .internet  import search
@@ -44,45 +43,26 @@ class Backend_Api:
         }
     
     def _conversation(self):
-        config = None
-        proxy = None
         try:
-            config = json.load(open("config.json","r",encoding="utf-8"))
-            proxy = config["proxy"]
-
-        except Exception:
-            pass
-
-        try:
-            jailbreak       = request.json['jailbreak']
-            internet_access = request.json['meta']['content']['internet_access']
-            conversation    = request.json['meta']['content']['conversation']
-            prompt          = request.json['meta']['content']['parts'][0]
+            #jailbreak       = request.json['jailbreak']
+            #internet_access = request.json['meta']['content']['internet_access']
+            #conversation    = request.json['meta']['content']['conversation']
+            prompt          = request.json['meta']['content']['parts']
             model           = request.json['model']
             provider        = request.json.get('provider').split('g4f.Provider.')[1]
 
-            messages = special_instructions[jailbreak] + conversation + search(internet_access, prompt) + [prompt]
+            messages = prompt
+            print(messages)
 
             def stream():
-                if proxy != None:
-                    yield from g4f.ChatCompletion.create(
-                        model=model,
-                        provider=get_provider(provider),
-                        messages=messages,
-                        stream=True,
-                        proxy=proxy
-                    ) if provider else g4f.ChatCompletion.create(
-                        model=model, messages=messages, stream=True, proxy=proxy
-                    )
-                else:
-                    yield from g4f.ChatCompletion.create(
-                        model=model,
-                        provider=get_provider(provider),
-                        messages=messages,
-                        stream=True,
-                    ) if provider else g4f.ChatCompletion.create(
-                        model=model, messages=messages, stream=True
-                    )
+                yield from g4f.ChatCompletion.create(
+                    model=g4f.models.gpt_35_long,
+                    provider=get_provider(provider),
+                    messages=messages,
+                    stream=True,
+                ) if provider else g4f.ChatCompletion.create(
+                    model=model, messages=messages, stream=True
+                )
 
             return self.app.response_class(stream(), mimetype='text/event-stream')
 
