be935e548779db1311fc01f100e03f3519f66152
abc <98614666+xtekky@users.noreply.github.com>
2023-11-08 09:05:39 +0000

    ~ | .

--- setup.py
+++ setup.py
@@ -5,71 +5,71 @@ from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as fh:
+    long_description = '\n' + fh.read()
 
-with open("requirements.txt") as f:
+with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-VERSION = "0.1.8.1"
+VERSION = '0.1.8.1'
 DESCRIPTION = (
-    "The official gpt4free repository | various collection of powerful language models"
+    'The official gpt4free repository | various collection of powerful language models'
 )
 
 # Setting up
 setup(
-    name="g4f",
+    name='g4f',
     version=VERSION,
-    author="Tekky",
-    author_email="<support@g4f.ai>",
+    author='Tekky',
+    author_email='<support@g4f.ai>',
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     long_description=long_description,
     packages=find_packages(),
     package_data={
-        "g4f": ["g4f/interference/*", "g4f/gui/client/*", "g4f/gui/server/*", "g4f/Provider/npm/*"]
+        'g4f': ['g4f/interference/*', 'g4f/gui/client/*', 'g4f/gui/server/*', 'g4f/Provider/npm/*']
     },
     include_package_data=True,
     install_requires=required,
     entry_points={
-        "console_scripts": ["g4f=g4f.cli:main"],
+        'console_scripts': ['g4f=g4f.cli:main'],
     },
-    url="https://github.com/xtekky/gpt4free",  # Link to your GitHub repository
+    url='https://github.com/xtekky/gpt4free',  # Link to your GitHub repository
     project_urls={
-        "Source Code": "https://github.com/xtekky/gpt4free",  # GitHub link
-        "Bug Tracker": "https://github.com/xtekky/gpt4free/issues",  # Link to issue tracker
+        'Source Code': 'https://github.com/xtekky/gpt4free',  # GitHub link
+        'Bug Tracker': 'https://github.com/xtekky/gpt4free/issues',  # Link to issue tracker
     },
     keywords=[
-        "python",
-        "chatbot",
-        "reverse-engineering",
-        "openai",
-        "chatbots",
-        "gpt",
-        "language-model",
-        "gpt-3",
-        "gpt3",
-        "openai-api",
-        "gpt-4",
-        "gpt4",
-        "chatgpt",
-        "chatgpt-api",
-        "openai-chatgpt",
-        "chatgpt-free",
-        "chatgpt-4",
-        "chatgpt4",
-        "chatgpt4-api",
-        "free",
-        "free-gpt",
-        "gpt4free",
-        "g4f",
+        'python',
+        'chatbot',
+        'reverse-engineering',
+        'openai',
+        'chatbots',
+        'gpt',
+        'language-model',
+        'gpt-3',
+        'gpt3',
+        'openai-api',
+        'gpt-4',
+        'gpt4',
+        'chatgpt',
+        'chatgpt-api',
+        'openai-chatgpt',
+        'chatgpt-free',
+        'chatgpt-4',
+        'chatgpt4',
+        'chatgpt4-api',
+        'free',
+        'free-gpt',
+        'gpt4free',
+        'g4f',
     ],
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
     ],
 )
