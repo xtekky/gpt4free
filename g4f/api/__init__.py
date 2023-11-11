7eb0972ab61fc7dca1ac8f1637a6e44ebcd63038
ThatLukinhasGuy <139662282+thatlukinhasguy1@users.noreply.github.com>
2023-11-10 21:35:31 -0300

    Update __init__.py

--- g4f/api/__init__.py
+++ g4f/api/__init__.py
@@ -1,7 +1,9 @@
 from fastapi            import FastAPI, Response, Request
+from fastapi.responses  import StreamingResponse
 from typing             import List, Union, Any, Dict, AnyStr
 from ._tokenizer        import tokenize
 from ..                 import BaseProvider
+from rich               import print
 
 import time
 import json
@@ -13,11 +15,13 @@ import g4f
 
 class Api:
     def __init__(self, engine: g4f, debug: bool = True, sentry: bool = False,
-                 list_ignored_providers: List[Union[str, BaseProvider]] = None) -> None:
+                 list_ignored_providers: List[Union[str, BaseProvider]] = None, key: str = None) -> None:
         self.engine = engine
         self.debug = debug
         self.sentry = sentry
         self.list_ignored_providers = list_ignored_providers
+
+        self.key = key
 
         self.app = FastAPI()
         nest_asyncio.apply()
@@ -26,16 +30,22 @@ class Api:
         JSONArray = List[Any]
         JSONStructure = Union[JSONArray, JSONObject]
 
-        @self.app.get("/")
-        async def read_root():
-            return Response(content=json.dumps({"info": "g4f API"}, indent=4), media_type="application/json")
+        @self.app.route("/", methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+        async def root(request: Request):
+            if self.debug:
+                print(f"[!] A request on the root endpoint was received from {request.client.host}.")
+            return Response(content=json.dumps({"detail": "g4f API"}, indent=4), media_type="application/json")
 
-        @self.app.get("/v1")
-        async def read_root_v1():
-            return Response(content=json.dumps({"info": "Go to /v1/chat/completions or /v1/models."}, indent=4), media_type="application/json")
+        @self.app.route("/v1", methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+        async def v1_root(request: Request):
+            if self.debug:
+                print(f"[!] A request on the V1 root endpoint was received from {request.client.host}.")
+            return Response(content=json.dumps({"detail": "Go to /v1/chat/completions or /v1/models."}, indent=4), media_type="application/json")
 
-        @self.app.get("/v1/models")
-        async def models():
+        @self.app.route("/v1/models", methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+        async def v1_models(request: Request):
+            if self.debug:
+                print(f"[!] A request on the model endpoint was received from {request.client.host}.")
             model_list = [{
                 'id': model,
                 'object': 'model',
@@ -46,8 +56,12 @@ class Api:
                 'object': 'list',
                 'data': model_list}, indent=4), media_type="application/json")
 
-        @self.app.get("/v1/models/{model_name}")
-        async def model_info(model_name: str):
+        @self.app.route("/v1/models/{model_name}", methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+        async def v1_model_info(request: Request):
+            model_name = request.url.path.split('/')[3]
+
+            if self.debug:
+                print(f"[!] A request on the model info endpoint was received from {request.client.host}.")
             try:
                 model_info = (g4f.ModelUtils.convert[model_name])
 
@@ -60,8 +74,25 @@ class Api:
             except:
                 return Response(content=json.dumps({"error": "The model does not exist."}, indent=4), media_type="application/json")
 
+
         @self.app.post("/v1/chat/completions")
-        async def chat_completions(request: Request, item: JSONStructure = None):
+        async def v1_chat_completions(request: Request, item: JSONStructure = None):
+            if self.debug:
+                print(f"[!] A request on the chat completion endpoint was received from {request.client.host}.")
+
+            if self.key != None:
+                try:
+                    auth_header = request.headers.get('Authorization').split(' ')
+                    if auth_header[1] != self.key:
+                        if self.debug:
+                            print(f"[!] The request from {request.client.host} failed because of authentication.")
+                        return Response(content=json.dumps({"error": "An invalid API key was provided."}, indent=4), media_type="application/json")
+                    
+                except:
+                    if self.debug:
+                        print(f"[!] The request from {request.client.host} failed because of authentication.")
+                    return Response(content=json.dumps({"error": "An exception occurred while validating your API key."}, indent=4), media_type="application/json")
+
             item_data = {
                 'model': 'gpt-3.5-turbo',
                 'stream': False,
@@ -75,6 +106,8 @@ class Api:
             try:
                 response = g4f.ChatCompletion.create(model=model, stream=stream, messages=messages)
             except:
+                if self.debug:
+                    print(f"[!] The request from {request.client.host} failed because of a provider-side error.")
                 return Response(content=json.dumps({"error": "An error occurred while generating the response."}, indent=4), media_type="application/json")
 
             completion_id = ''.join(random.choices(string.ascii_letters + string.digits, k=28))
@@ -105,6 +138,9 @@ class Api:
                         'total_tokens': prompt_tokens + completion_tokens,
                     },
                 }
+
+                if self.debug:
+                    print(f"[!] The request from {request.client.host} succeeded.")
 
                 return Response(content=json.dumps(json_data, indent=4), media_type="application/json")
 
@@ -151,12 +187,18 @@ class Api:
                 except GeneratorExit:
                     pass
 
-            return Response(content=json.dumps(streaming(), indent=4), media_type="application/json")
+            if self.debug:
+                print(f"[!] The request from {request.client.host} succeeded.")
 
-        @self.app.post("/v1/completions")
-        async def completions():
-            return Response(content=json.dumps({'info': 'Not working yet.'}, indent=4), media_type="application/json")
+            return StreamingResponse(streaming(), media_type="text/event-stream")
+
+        @self.app.route("/v1/completions", methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+        async def completions(request: Request):
+            if self.debug:
+                print(f"[!] A request on the completions endpoint was received from {request.client.host}.")
+            return Response(content=json.dumps({'detail': 'Not working yet.'}, indent=4), media_type="application/json")
 
     def run(self, ip):
         split_ip = ip.split(":")
-        uvicorn.run(app=self.app, host=split_ip[0], port=int(split_ip[1]), use_colors=False)
+        print(f"[!] The g4f API is now running on http://{ip}/")
+        uvicorn.run(app=self.app, host=split_ip[0], port=int(split_ip[1]), use_colors=False, log_level="error")
