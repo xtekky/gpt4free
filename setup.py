041f9b878106d49abd4a1ce86aaa19a4745a2cbe
abc <98614666+xtekky@users.noreply.github.com>
2024-01-11 22:41:32 +0000

    ~ | g4f v-0.2.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as fh:
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-VERSION = '0.2.0.0'
+VERSION = '0.2.0.1'
 DESCRIPTION = (
     'The official gpt4free repository | various collection of powerful language models'
 )
