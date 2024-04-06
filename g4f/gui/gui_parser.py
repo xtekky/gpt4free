38c429ec3831a6ebec766c1cb579d0e3f34ab5b5
H Lohaus <hlohaus@users.noreply.github.com>
2024-04-06 16:25:11 +0200

    Update gui

---
+++ g4f/gui/gui_parser.py
@@ -0,0 +1,8 @@
+from argparse import ArgumentParser
+
+def gui_parser():
+    parser = ArgumentParser(description="Run the GUI")
+    parser.add_argument("-host", type=str, default="0.0.0.0", help="hostname")
+    parser.add_argument("-port", type=int, default=8080, help="port")
+    parser.add_argument("-debug", action="store_true", help="debug mode")
+    return parser
