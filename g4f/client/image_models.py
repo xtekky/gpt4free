3f17f9b8e2fc5ed4f4bd499d825bdb1754e13377
H Lohaus <hlohaus@users.noreply.github.com>
2024-04-06 21:24:48 +0200

    Create image_models.py

---
+++ g4f/client/image_models.py
@@ -0,0 +1,11 @@
+class ImageModels():
+    gemini = Gemini
+    openai = OpenaiChat
+    you = You
+
+    def __init__(self, client: Client) -> None:
+        self.client = client
+        self.default = BingCreateImages(proxy=self.client.get_proxy())
+
+    def get(self, name: str, default: ImageProvider = None) -> ImageProvider:
+        return getattr(self, name) if hasattr(self, name) else default or self.default
