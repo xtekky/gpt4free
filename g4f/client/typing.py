7339cd7241f4b90a599a104a01d122ade7e6d8fe
H Lohaus <hlohaus@users.noreply.github.com>
2024-04-06 20:31:36 +0200

    Delete g4f/client/typing.py

--- g4f/client/typing.py
+++
@@ -1,5 +0,0 @@
-from ..providers.types import BaseProvider, ProviderType
-from typing import Union, Iterator
-ImageProvider = Union[BaseProvider, object]
-Proxies = Union[dict, str]
-IterResponse = Iterator[Union[ChatCompletion, ChatCompletionChunk]]
