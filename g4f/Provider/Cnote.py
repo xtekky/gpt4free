05ba43e47c46a8f4745e484dbad1e62b524a52c0
kqlio67 <166700875+kqlio67@users.noreply.github.com>
2024-04-12 18:11:30 +0000

    Update Cnote.py

--- g4f/Provider/Cnote.py
+++ g4f/Provider/Cnote.py
@@ -23,7 +23,7 @@ class Cnote(AsyncGeneratorProvider):
         **kwargs
     ) -> AsyncResult:
         headers = {
-            "User-Agent": "Mozilla/9.0 (Macintosh; macOS) AppleWebKit (compatible) (KHTML, Gecko)",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
             "Accept": "application/json, text/plain, */*",
             "Accept-Language": "en-US,en;q=0.5",
             "Accept-Encoding": "gzip, deflate, br",
