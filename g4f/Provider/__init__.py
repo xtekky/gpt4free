189940701eb7c3958dfb8198f4e2bb3429f8d5e9
pratham-darooka <pdarooka1011@gmail.com>
2024-04-19 00:04:17 +0530

    Added Meta llama-3 support! Decided to change llama2.py to llama.py to hold all the llama family models.

--- g4f/Provider/__init__.py
+++ g4f/Provider/__init__.py
@@ -38,7 +38,7 @@ from .HuggingChat      import HuggingChat
 from .HuggingFace      import HuggingFace
 from .Koala            import Koala
 from .Liaobots         import Liaobots
-from .Llama2           import Llama2
+from .Llama           import Llama2
 from .Local            import Local
 from .PerplexityLabs   import PerplexityLabs
 from .Pi               import Pi
