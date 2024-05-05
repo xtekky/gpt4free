a03db91caace96d26d761da5a8ff56ca0c270bde
hdsz25 <hdsz25@hotmail.com>
2024-05-05 11:00:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ DESCRIPTION = (
 # Setting up
 setup(
     name='g4f',
-    version="0.3.0.8.dan",
+    version="0.3.0.8.dev",
     author='Tekky',
     author_email='<support@g4f.ai>',
     description=DESCRIPTION,
