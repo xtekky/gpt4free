4fe31264e4a4c91fd44fd058c324485753b9d583
kqlio67 <kqlio67@users.noreply.github.com>
2024-09-06 17:43:13 +0300

    refactor(test): simplify model test cases to core options

--- etc/testing/test_all.py
+++ etc/testing/test_all.py
@@ -38,21 +38,11 @@ async def test(model: g4f.Model):
 
 async def start_test():
     models_to_test = [
-        # GPT-3.5 4K Context
+        # GPT-3.5
         g4f.models.gpt_35_turbo,
-        g4f.models.gpt_35_turbo_0613,
 
-        # GPT-3.5 16K Context
-        g4f.models.gpt_35_turbo_16k,
-        g4f.models.gpt_35_turbo_16k_0613,
-
-        # GPT-4 8K Context
+        # GPT-4
         g4f.models.gpt_4,
-        g4f.models.gpt_4_0613,
-
-        # GPT-4 32K Context
-        g4f.models.gpt_4_32k,
-        g4f.models.gpt_4_32k_0613,
     ]
 
     models_working = []
