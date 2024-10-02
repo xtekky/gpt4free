1e859b683dcab7d384e3e2f5c26f8512366a59f5
kqlio67 <kqlio67@users.noreply.github.com>
2024-10-03 01:03:05 +0300

    refactor(g4f/Provider/Blackbox.py): update model names and adjust payload

--- g4f/Provider/Blackbox.py
+++ g4f/Provider/Blackbox.py
@@ -23,20 +23,11 @@ class Blackbox(AsyncGeneratorProvider, ProviderModelMixin):
         'gemini-1.5-flash',
         "llama-3.1-8b",
         'llama-3.1-70b',
-        'llama-3.1-405b',
         'ImageGenerationLV45LJp',
-        'GPT-4o',
-        'Gemini-PRO',
-        'Claude-Sonnet-3.5',
+        'gpt-4o',
+        'gemini-pro',
+        'claude-sonnet-3.5',
     ]
-    
-    model_aliases = {
-        "gemini-flash": "gemini-1.5-flash",
-        "flux": "ImageGenerationLV45LJp",
-        "gpt-4o": "GPT-4o",
-        "gemini-pro": "Gemini-PRO",
-        "claude-3.5-sonnet": "Claude-Sonnet-3.5",
-    }
 
     agentMode = {
         'ImageGenerationLV45LJp': {'mode': True, 'id': "ImageGenerationLV45LJp", 'name': "Image Generation"},
@@ -47,13 +38,17 @@ class Blackbox(AsyncGeneratorProvider, ProviderModelMixin):
         "gemini-1.5-flash": {'mode': True, 'id': 'Gemini'},
         "llama-3.1-8b": {'mode': True, 'id': "llama-3.1-8b"},
         'llama-3.1-70b': {'mode': True, 'id': "llama-3.1-70b"},
-        'llama-3.1-405b': {'mode': True, 'id': "llama-3.1-405b"},
     }
     
     userSelectedModel = {
-        "GPT-4o": "GPT-4o",
-        "Gemini-PRO": "Gemini-PRO",
-        'Claude-Sonnet-3.5': "Claude-Sonnet-3.5",
+        "gpt-4o": "gpt-4o",
+        "gemini-pro": "gemini-pro",
+        'claude-sonnet-3.5': "claude-sonnet-3.5",
+    }
+    
+    model_aliases = {
+        "gemini-flash": "gemini-1.5-flash",
+        "flux": "ImageGenerationLV45LJp",
     }
 
     @classmethod
@@ -119,8 +114,9 @@ class Blackbox(AsyncGeneratorProvider, ProviderModelMixin):
                 "agentMode": {},
                 "trendingAgentMode": {},
                 "userSelectedModel": None,
+                "userSystemPrompt": None,
                 "isMicMode": False,
-                "maxTokens": 99999999,
+                "maxTokens": 1024,
                 "playgroundTopP": 0.9,
                 "playgroundTemperature": 0.5,
                 "isChromeExt": False,
