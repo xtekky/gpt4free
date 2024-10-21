6a3684a7b21c2275a5ba38ed98fc904aced2a5fc
kqlio67 <kqlio67@users.noreply.github.com>
2024-10-21 20:01:10 +0300

    Restored provider (g4f/Provider/nexra/NexraBing.py)

--- g4f/models.py
+++ g4f/models.py
@@ -39,6 +39,7 @@ from .Provider import (
     Liaobots,
     MagickPen,
     MetaAI,
+    NexraBing,
     NexraBlackbox,
     NexraChatGPT,
     NexraChatGPT4o,
@@ -150,7 +151,7 @@ gpt_4_turbo = Model(
 gpt_4 = Model(
     name          = 'gpt-4',
     base_provider = 'OpenAI',
-    best_provider = IterListProvider([Chatgpt4Online, Ai4Chat, NexraChatGPT, NexraChatGptV2, NexraChatGptWeb, Airforce, Bing, OpenaiChat, gpt_4_turbo.best_provider, gpt_4o.best_provider, gpt_4o_mini.best_provider])
+    best_provider = IterListProvider([Chatgpt4Online, Ai4Chat, NexraBing, NexraChatGPT, NexraChatGptV2, NexraChatGptWeb, Airforce, Bing, OpenaiChat, gpt_4_turbo.best_provider, gpt_4o.best_provider, gpt_4o_mini.best_provider])
 )
 
 # o1
