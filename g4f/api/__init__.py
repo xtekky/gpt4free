891833c49881d210f74e30ede2926beba4578089
foxfire52 <foxfire52@virgilio.it>
2024-10-21 11:50:22 +0200

    Update __init__.py

--- g4f/api/__init__.py
+++ g4f/api/__init__.py
@@ -21,6 +21,10 @@ from g4f.client import Client
 from g4f.typing import Messages
 from g4f.cookies import read_cookie_files
 
+logger = logging.getLogger(__main__)
+logger.setLevel(logging.ERROR)
+
+
 def create_app():
     app = FastAPI()
     api = Api(app)
@@ -174,13 +178,13 @@ class Api:
                     except GeneratorExit:
                         pass
                     except Exception as e:
-                        logging.exception(e)
+                        logger.exception(e)
                         yield f'data: {format_exception(e, config)}\n\n'
                     yield "data: [DONE]\n\n"
                 return StreamingResponse(streaming(), media_type="text/event-stream")
 
             except Exception as e:
-                logging.exception(e)
+                logger.exception(e)
                 return Response(content=format_exception(e, config), status_code=500, media_type="application/json")
 
         @self.app.post("/v1/completions")
@@ -203,7 +207,7 @@ class Api:
                 )
                 return JSONResponse(response.to_json())
             except Exception as e:
-                logging.exception(e)
+                logger.exception(e)
                 return Response(content=format_exception(e, config), status_code=500, media_type="application/json")
 
 def format_exception(e: Exception, config: ChatCompletionsForm) -> str:
