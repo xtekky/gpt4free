e57b221c343287f33cb666ddef5246c6302dff15
Heiner Lohaus <hlohaus@users.noreply.github.com>
2024-11-19 17:32:42 +0100

    Fix aliases to gui  arguments

--- g4f/gui/gui_parser.py
+++ g4f/gui/gui_parser.py
@@ -3,11 +3,7 @@ from argparse import ArgumentParser
 def gui_parser():
     parser = ArgumentParser(description="Run the GUI")
     parser.add_argument("--host", type=str, default="0.0.0.0", help="hostname")
-    parser.add_argument_alias('-h', '--host')
-    parser.add_argument("--port", type=int, default=8080, help="port")
-    parser.add_argument_alias('-p', '--port')
-    parser.add_argument("--debug", action="store_true", help="debug mode")
-    parser.add_argument_alias('-d', '--debug')
-    parser.add_argument_alias('-debug', '--debug')
+    parser.add_argument("--port", "-p", type=int, default=8080, help="port")
+    parser.add_argument("--debug", "-d", "-debug", action="store_true", help="debug mode")
     parser.add_argument("--ignore-cookie-files", action="store_true", help="Don't read .har and cookie files.")
     return parser
