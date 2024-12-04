b56128e3bab41bb44165c17d32e1ee24d7d43232
kqlio67 <>
2024-12-04 13:24:57 +0200

    Update g4f/Provider/DeepInfraChat.py

--- g4f/Provider/DeepInfraChat.py
+++ g4f/Provider/DeepInfraChat.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ClientResponseError
 import json
-
 from ..typing import AsyncResult, Messages, ImageType
 from .base_provider import AsyncGeneratorProvider, ProviderModelMixin
+
 
 class DeepInfraChat(AsyncGeneratorProvider, ProviderModelMixin):
     url = "https://deepinfra.com/chat"
@@ -13,7 +13,7 @@ class DeepInfraChat(AsyncGeneratorProvider, ProviderModelMixin):
     supports_stream = True
     supports_system_message = True
     supports_message_history = True
-    
+
     default_model = 'meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo'
     models = [
         'meta-llama/Meta-Llama-3.1-8B-Instruct',
@@ -34,16 +34,6 @@ class DeepInfraChat(AsyncGeneratorProvider, ProviderModelMixin):
         "nemotron-70b": "nvidia/Llama-3.1-Nemotron-70B-Instruct",
     }
 
-
-    @classmethod
-    def get_model(cls, model: str) -> str:
-        if model in cls.models:
-            return model
-        elif model in cls.model_aliases:
-            return cls.model_aliases[model]
-        else:
-            return cls.default_model
-
     @classmethod
     async def create_async_generator(
         cls,
@@ -54,34 +44,22 @@ class DeepInfraChat(AsyncGeneratorProvider, ProviderModelMixin):
         image_name: str = None,
         **kwargs
     ) -> AsyncResult:
-        model = cls.get_model(model)
-        
         headers = {
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive',
             'Content-Type': 'application/json',
             'Origin': 'https://deepinfra.com',
-            'Pragma': 'no-cache',
             'Referer': 'https://deepinfra.com/',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-site',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
-            'X-Deepinfra-Source': 'web-embed',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+            'X-Deepinfra-Source': 'web-page',
             'accept': 'text/event-stream',
-            'sec-ch-ua': '"Not;A=Brand";v="24", "Chromium";v="128"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Linux"',
         }
         
-        async with ClientSession(headers=headers) as session:
-            data = {
-                'model': model,
-                'messages': messages,
-                'stream': True
-            }
+        data = {
+            'model': model,
+            'messages': messages,
+            'stream': True
+        }
 
+        async with ClientSession(headers=headers) as session:
             async with session.post(cls.api_endpoint, json=data, proxy=proxy) as response:
                 response.raise_for_status()
                 async for line in response.content:
