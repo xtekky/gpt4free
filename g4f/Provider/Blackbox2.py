5d71fd242680ecbd000eddf9a8342e6b82d6f9e7
kqlio67 <>
2024-12-15 22:01:42 +0200

    Update g4f/Provider/Blackbox2.py

--- g4f/Provider/Blackbox2.py
+++ g4f/Provider/Blackbox2.py
@@ -185,7 +185,6 @@ class Blackbox2(AsyncGeneratorProvider, ProviderModelMixin):
             'accept-language': 'en-US,en;q=0.9',
             'content-type': 'text/plain;charset=UTF-8',
             'origin': 'https://www.blackbox.ai',
-            'priority': 'u=1, i',
             'referer': 'https://www.blackbox.ai',
             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
         }
