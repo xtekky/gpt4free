da6df9dbf0a62fec39edbb264c29aa0449c794b6
kqlio67 <>
2025-03-21 21:40:14 +0200

    Update g4f/Provider/DeepInfraChat.py

--- g4f/Provider/DeepInfraChat.py
+++ g4f/Provider/DeepInfraChat.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from ..typing import AsyncResult, Messages
 from .template import OpenaiTemplate
 
 class DeepInfraChat(OpenaiTemplate):
@@ -58,36 +57,3 @@ class DeepInfraChat(OpenaiTemplate):
         "mixtral-8x22b": "mistralai/Mixtral-8x22B-Instruct-v0.1",
         "minicpm-2.5": "openbmb/MiniCPM-Llama3-V-2_5",
     }
-
-    @classmethod
-    async def create_async_generator(
-        cls,
-        model: str,
-        messages: Messages,
-        stream: bool = True,
-        top_p: float = 0.9,
-        temperature: float = 0.7,
-        max_tokens: int = None,
-        headers: dict = {},
-        **kwargs
-    ) -> AsyncResult:
-        headers = {
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Origin': 'https://deepinfra.com',
-            'Referer': 'https://deepinfra.com/',
-            'X-Deepinfra-Source': 'web-page',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-            **headers
-        }
-
-        async for chunk in super().create_async_generator(
-            model,
-            messages,
-            headers=headers,
-            stream=stream,
-            top_p=top_p,
-            temperature=temperature,
-            max_tokens=max_tokens,
-            **kwargs
-        ):
-            yield chunk
