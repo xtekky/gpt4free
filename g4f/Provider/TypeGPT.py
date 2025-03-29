494746302a51df26bf4fe9e2e722bbd386dd40aa
hlohaus <983577+hlohaus@users.noreply.github.com>
2025-03-29 20:11:32 +0100

    Update models.py

--- g4f/Provider/TypeGPT.py
+++ g4f/Provider/TypeGPT.py
@@ -26,11 +26,11 @@ class TypeGPT(OpenaiTemplate):
     default_model = 'gpt-4o-mini-2024-07-18'
     default_vision_model = default_model
     vision_models = ['gpt-3.5-turbo', 'gpt-3.5-turbo-202201', default_vision_model, "o3-mini"]
-    fallback_models = vision_models + ["deepseek-r1", "deepseek-v3", "evil", "o1"]
+    fallback_models = vision_models + ["deepseek-r1", "deepseek-v3", "evil"]
     image_models = ["Image-Generator"]
     model_aliases = {
-        "gpt-3.5-turbo": "gpt-3.5-turbo-202201",
         "gpt-4o-mini": "gpt-4o-mini-2024-07-18",
+        "evil": "uncensored-r1",
     }
 
     @classmethod
