856a63e66d70f923cc6c183bc2f8049f8bb55721
H Lohaus <hlohaus@users.noreply.github.com>
2025-06-13 04:38:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(current_dir, 'README.md')) as f:
+with open(os.path.join(current_dir, 'README.md'), "r") as f:
     long_description = f.read()
 
 long_description = long_description.replace("[!NOTE]", "")
