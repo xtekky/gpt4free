c5670047b6c9c3dc9522da56fc17ede202ae7862
H Lohaus <hlohaus@users.noreply.github.com>
2025-10-02 08:20:23 +0200

    Update OpenaiTemplate.py

--- g4f/Provider/template/OpenaiTemplate.py
+++ g4f/Provider/template/OpenaiTemplate.py
@@ -127,7 +127,7 @@ class OpenaiTemplate(AsyncGeneratorProvider, ProviderModelMixin, RaiseErrorMixin
 
             if stream:
                 kwargs.setdefault("stream_options", {"include_usage": True})
-            extra_parameters = {key: kwargs[key] for key in extra_parameters if key in kwargs}
+            extra_parameters = filter_none(**{key: kwargs.get(key) for key in extra_parameters})
             if extra_body is None:
                 extra_body = {}
             data = filter_none(
