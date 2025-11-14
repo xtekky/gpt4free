36e66950caed544a915b771ebe76b1e05cf5b598
hlohaus <983577+hlohaus@users.noreply.github.com>
2025-11-14 16:51:12 +0100

    Update default model to "gpt-5-1" and enhance text model list

--- g4f/Provider/openai/models.py
+++ g4f/Provider/openai/models.py
@@ -1,10 +1,11 @@
-default_model = "auto"
+default_model = "gpt-5-1"
 default_image_model = "gpt-image"
 image_models = [default_image_model]
-text_models = [default_model, "gpt-5", "gpt-5-instant", "gpt-5-thinking", "gpt-4", "gpt-4.1", "gpt-4.1-mini", "gpt-4.5", "gpt-4o", "gpt-4o-mini", "o1", "o1-mini", "o3-mini", "o3-mini-high", "o4-mini", "o4-mini-high"]
+text_models = [default_model, "gpt-5-1-instant", "gpt-5-1-thinking", "gpt-5", "gpt-5-instant", "gpt-5-thinking", "gpt-4", "gpt-4.1", "gpt-4.1-mini", "gpt-4.5", "gpt-4o", "gpt-4o-mini", "o1", "o1-mini", "o3-mini", "o3-mini-high", "o4-mini", "o4-mini-high"]
 vision_models = text_models
 models = text_models + image_models
 model_aliases = {
+    "gpt-5.1": "gpt-5-1",
     "gpt-4.1": "gpt-4-1",
     "gpt-4.1-mini": "gpt-4-1-mini",
     "gpt-4.5": "gpt-4-5",
